d17c6316e4b58c4f533bd84d1b3714e15b6f4aa6
ahyahya1616 <ahmaneyahya10@gmail.com>
2025-07-29 12:05:17 +0100

    modification de server.js

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,21 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+const connectDB = require("./config/db"); // Import de la connexion
 
 const app = express();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log("MongoDB connecté"))
-  .catch((err) => console.error("Erreur Mongo:", err));
+// Connexion à MongoDB
+connectDB();
 
-// Exemple route
-app.get("/api/test", (req, res) => {
-  res.json({ message: "Le backend fonctionne" });
-});
 
+
+// Lancement du serveur
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Serveur lancé sur le port ${PORT}`);
+  console.log(`🚀 Serveur lancé sur le port ${PORT}`);
 });
