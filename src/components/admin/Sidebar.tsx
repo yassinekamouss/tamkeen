cbe1e794e8a0fefd1262f09d8bcf0df472e52a4d
Votre Nom <mohammedsalhisam@gmail.com>
2025-08-04 12:42:28 +0100

    sidebar

--- src/components/admin/Sidebar.tsx
+++ src/components/admin/Sidebar.tsx
@@ -1,15 +1,18 @@
 // src/components/Sidebar.tsx
-import { NavLink , useNavigate} from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
-const Sidebar = () => {
-    const navigate = useNavigate();
+const Sidebar = ({ isOpen }: { isOpen: boolean }) => {
+  const navigate = useNavigate();
 
   const handleLogout = () => {
     localStorage.removeItem("adminToken");  // Supprime le token
     navigate("/admin/login");                // Redirige vers la page de login
   };
-  return (
-    <div className="w-64 h-screen bg-gray-800 text-white p-5 space-y-6 fixed">
+return (
+    <div
+      className={`h-screen bg-gray-800 text-white p-5 space-y-6 fixed top-0 left-0 z-50 transition-all duration-300 ease-in-out
+        ${isOpen ? "translate-x-0 w-64" : "-translate-x-full w-64"} overflow-hidden`}
+    >
       <h2 className="text-2xl font-bold mb-8">Admin</h2>
       <nav className="flex flex-col space-y-4">
         <NavLink
@@ -37,7 +40,7 @@ const Sidebar = () => {
           Programmes
         </NavLink>
 
-         <button
+        <button
           onClick={handleLogout}
           className="mt-8 text-left text-red-500 hover:text-red-700"
         >
--- src/pages/admin/Dashboard.tsx
+++ src/pages/admin/Dashboard.tsx
@@ -2,10 +2,8 @@ import React from "react";
 
 const Dashboard: React.FC = () => {
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Tableau de bord</h1>
-      <p>Bienvenue sur le tableau de bord administrateur.</p>
-    </div>
+      <h1 className="text-2xl font-bold ">Tableau de bord</h1>
+
   );
 };
 
