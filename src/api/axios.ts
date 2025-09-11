c7eba30f1631656357ce94376193b2d3af66e407
ahyahya1616 <ahmaneyahya10@gmail.com>
2025-09-11 03:13:54 +0100

    Update axios.ts

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
 
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "/api" // via le proxy Vercel
+    : "http://localhost:5000/api"; // backend local
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_API_URL || "http://localhost:5000/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  withCredentials: true, 
+  baseURL,
+  withCredentials: true, // pour envoyer les cookies
 });
 
 export default api;
