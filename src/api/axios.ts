d591d826bfeb9e30b16624eeb598e5dd2e6c5d9c
ahyahya1616 <ahmaneyahya10@gmail.com>
2025-09-16 23:27:46 +0100

    modification de axios.ts et Hero.tsx

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 
-const baseURL = typeof window !== "undefined" && window.location.hostname === "localhost"
-  ? "http://localhost:5000/api"
-  : "/api";
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "/api" // via le proxy Vercel
+    : "http://localhost:5000/api"; // backend local
 
+    
 const api = axios.create({
   baseURL,
   withCredentials: true,
