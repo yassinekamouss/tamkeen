138178b2c94d330dcc69228aa1f068467fa94113
ahyahya1616 <ahmaneyahya10@gmail.com>
2025-09-16 23:50:58 +0100

    modification de axious.ts

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const baseURL =
-  process.env.NODE_ENV === "production"
+const baseURL = import.meta.env.NODE_ENV === "production"
     ? "/api" // via le proxy Vercel
     : "http://localhost:5000/api"; // backend local
 
