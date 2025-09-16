6c86870aa5d7a0d1f7e2e5db12bc75b1cf45ce15
ahyahya1616 <ahmaneyahya10@gmail.com>
2025-09-17 00:56:24 +0100

    add social media links in the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -124,9 +124,12 @@ const Footer: React.FC = () => {
               </h4>
               <div className="flex space-x-3">
                 <a
-                  href="#"
+                  href="https://www.facebook.com/share/16W7DLrytf/?mibextid=wwXIfr"
                   className="text-gray-400 hover:text-blue-400 transition-colors duration-300"
-                  aria-label="Facebook">
+                  aria-label="Facebook"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  >
                   <svg
                     className="w-5 h-5"
                     fill="currentColor"
@@ -135,9 +138,12 @@ const Footer: React.FC = () => {
                   </svg>
                 </a>
                 <a
-                  href="#"
+                  href="https://www.linkedin.com/company/tamkeen-center/"
                   className="text-gray-400 hover:text-blue-400 transition-colors duration-300"
-                  aria-label="LinkedIn">
+                  aria-label="LinkedIn"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  >
                   <svg
                     className="w-5 h-5"
                     fill="currentColor"
@@ -145,17 +151,22 @@ const Footer: React.FC = () => {
                     <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
                   </svg>
                 </a>
-                <a
-                  href="#"
-                  className="text-gray-400 hover:text-blue-400 transition-colors duration-300"
-                  aria-label="Twitter">
-                  <svg
-                    className="w-5 h-5"
-                    fill="currentColor"
-                    viewBox="0 0 24 24">
-                    <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" />
-                  </svg>
-                </a>
+              <a
+              href="https://www.instagram.com/tamkeen_center_consulting?igsh=NjllczJzNDRsdWRq"
+              className="text-gray-400 hover:text-pink-500 transition-colors duration-300"
+              aria-label="Instagram"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <svg
+                className="w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path d="M7.75 2h8.5A5.75 5.75 0 0 1 22 7.75v8.5A5.75 5.75 0 0 1 16.25 22h-8.5A5.75 5.75 0 0 1 2 16.25v-8.5A5.75 5.75 0 0 1 7.75 2zm0 1.5A4.25 4.25 0 0 0 3.5 7.75v8.5A4.25 4.25 0 0 0 7.75 20.5h8.5A4.25 4.25 0 0 0 20.5 16.25v-8.5A4.25 4.25 0 0 0 16.25 3.5h-8.5zm9 1.75a.75.75 0 0 1 .75.75v1.25a.75.75 0 0 1-1.5 0V6a.75.75 0 0 1 .75-.75zM12 7a5 5 0 1 1 0 10 5 5 0 0 1 0-10zm0 1.5a3.5 3.5 0 1 0 0 7 3.5 3.5 0 0 0 0-7z" />
+              </svg>
+            </a>
+
               </div>
             </div>
           </div>
