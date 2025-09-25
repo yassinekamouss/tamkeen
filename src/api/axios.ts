3d5ddd15ac3a4de81a0620b8f2d9286f7d9bdf96
yassinekamouss <yassinekamouss76@gmail.com>
2025-09-25 16:31:20 +0100

    deleting the lazy loanding mode from the home page.

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -4,7 +4,7 @@ const baseURL = import.meta.env.NODE_ENV === "production"
     ? "/api" // via le proxy Vercel
     : "http://localhost:5000/api"; // backend local
 
-    
+
 const api = axios.create({
   baseURL,
   withCredentials: true,
