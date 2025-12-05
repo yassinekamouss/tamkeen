53220eec8ff05e050b68c321887791d77a73a337
Votre Nom <mohammedsalhisam@gmail.com>
2025-12-05 21:39:22 +0100

    footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -102,7 +102,7 @@ const Footer: React.FC = () => {
                   {t('footer.address')}
                 </span>
               </div>
-              <div className="flex items-center">
+              {/* <div className="flex items-center">
                 <svg
                   className="w-4 h-4 mr-2 text-blue-400 flex-shrink-0"
                   fill="currentColor"
@@ -114,7 +114,7 @@ const Footer: React.FC = () => {
                   className="text-gray-300 hover:text-white transition-colors duration-300 text-xs sm:text-sm">
                   {t('footer.phone')}
                 </a>
-              </div>
+              </div> */}
             </div>
 
             {/* Réseaux sociaux */}
@@ -129,7 +129,7 @@ const Footer: React.FC = () => {
                   aria-label="Facebook"
                   target="_blank"
                   rel="noopener noreferrer"
-                  >
+                >
                   <svg
                     className="w-5 h-5"
                     fill="currentColor"
@@ -143,7 +143,7 @@ const Footer: React.FC = () => {
                   aria-label="LinkedIn"
                   target="_blank"
                   rel="noopener noreferrer"
-                  >
+                >
                   <svg
                     className="w-5 h-5"
                     fill="currentColor"
@@ -151,21 +151,21 @@ const Footer: React.FC = () => {
                     <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
                   </svg>
                 </a>
-              <a
-              href="https://www.instagram.com/tamkeen_center_consulting?igsh=NjllczJzNDRsdWRq"
-              className="text-gray-400 hover:text-pink-500 transition-colors duration-300"
-              aria-label="Instagram"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <svg
-                className="w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.75 2h8.5A5.75 5.75 0 0 1 22 7.75v8.5A5.75 5.75 0 0 1 16.25 22h-8.5A5.75 5.75 0 0 1 2 16.25v-8.5A5.75 5.75 0 0 1 7.75 2zm0 1.5A4.25 4.25 0 0 0 3.5 7.75v8.5A4.25 4.25 0 0 0 7.75 20.5h8.5A4.25 4.25 0 0 0 20.5 16.25v-8.5A4.25 4.25 0 0 0 16.25 3.5h-8.5zm9 1.75a.75.75 0 0 1 .75.75v1.25a.75.75 0 0 1-1.5 0V6a.75.75 0 0 1 .75-.75zM12 7a5 5 0 1 1 0 10 5 5 0 0 1 0-10zm0 1.5a3.5 3.5 0 1 0 0 7 3.5 3.5 0 0 0 0-7z" />
-              </svg>
-            </a>
+                <a
+                  href="https://www.instagram.com/tamkeen_center_consulting?igsh=NjllczJzNDRsdWRq"
+                  className="text-gray-400 hover:text-pink-500 transition-colors duration-300"
+                  aria-label="Instagram"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <svg
+                    className="w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M7.75 2h8.5A5.75 5.75 0 0 1 22 7.75v8.5A5.75 5.75 0 0 1 16.25 22h-8.5A5.75 5.75 0 0 1 2 16.25v-8.5A5.75 5.75 0 0 1 7.75 2zm0 1.5A4.25 4.25 0 0 0 3.5 7.75v8.5A4.25 4.25 0 0 0 7.75 20.5h8.5A4.25 4.25 0 0 0 20.5 16.25v-8.5A4.25 4.25 0 0 0 16.25 3.5h-8.5zm9 1.75a.75.75 0 0 1 .75.75v1.25a.75.75 0 0 1-1.5 0V6a.75.75 0 0 1 .75-.75zM12 7a5 5 0 1 1 0 10 5 5 0 0 1 0-10zm0 1.5a3.5 3.5 0 1 0 0 7 3.5 3.5 0 0 0 0-7z" />
+                  </svg>
+                </a>
 
               </div>
             </div>
